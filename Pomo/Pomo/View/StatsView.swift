a138de0b23c68a346546d467fa8f1bb79b9735ad
João Gabriel Antunes <jg.antunes.medeiros@gmail.com>
2020-11-10 18:21:20 -0300

    [FEATURE]UnBArqDsw/2020.1_G6_Pomo#52 Modificando o Swift Package para contemplar escopo do projeto.

--- Pomo/Pomo/View/StatsView.swift
+++ Pomo/Pomo/View/StatsView.swift
@@ -15,25 +15,27 @@ struct StatsView: View {
     
     var columns = [
         GridItem(.adaptive(minimum: 120)),
-        GridItem(.adaptive(minimum: 120))
+        GridItem(.adaptive(minimum: 150))
     ]
     
     var body: some View {
         NavigationView {
             ScrollView {
-                VStack {
-                    Text("Stats Go Here!")
-                    LazyVGrid(columns: columns) {
-                        ForEach(sessionItems, id: \.self) { item in
-                            LineChartView(data: [10, 12, 9, 2, 5, 13, 16], title: item.name!, legend: item.sessionDescription!, dropShadow: false)
-                                
-                        }
-                    }
-                    
+                VStack(spacing: 20) {
+                    ForEach(sessionItems, id: \.self) { item in
+                        LineChartView(data: [10, 12, 9, 2, 5, 13, 16], title: item.name!, legend: item.sessionDescription!, style: ChartStyle(
+                                        backgroundColor: Color.orange,
+                                        accentColor: Colors.OrangeStart,
+                                        secondGradientColor: Colors.OrangeEnd,
+                                        textColor: Color.white,
+                                        legendTextColor: Color.white.opacity(0.75),
+                                        dropShadowColor: Color.gray), dropShadow: false)
                         
+                    }
                 }
                 .navigationTitle("Stats")
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding()
             }
         }
     }
