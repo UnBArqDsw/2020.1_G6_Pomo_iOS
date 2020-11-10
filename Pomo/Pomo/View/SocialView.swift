93a2845f4c47b21253f895c3c9404190bab5c092
João Gabriel Antunes <jg.antunes.medeiros@gmail.com>
2020-11-09 21:43:01 -0300

    [FEATURE]UnBArqDsw/2020.1_G6_Pomo#52 Atualizando SocialView e adicinoando acesso ao ChatView, com animações e gestos.

--- Pomo/Pomo/View/SocialView.swift
+++ Pomo/Pomo/View/SocialView.swift
@@ -12,17 +12,39 @@ import SwiftUI
 struct SocialView: View {
     @State private var pageIndex = 0
     var body: some View {
-        VStack {
-            NavigationView {
-                Text("SocialView")
-                    .navigationTitle("Social")
-                    .navigationBarItems(trailing: Button(action: {}) {
+        TabView(selection: $pageIndex) {
+            VStack() {
+                HStack {
+                    Text("Social")
+                        .font(.largeTitle).bold()
+                        .offset(y: 32)
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        withAnimation {
+                            pageIndex = 1
+                        }
+                    }) {
                         Image(systemName: "paperplane")
                             .font(.title)
                             .foregroundColor(.primary)
-                    })
+                    }
+                    .offset(y: -10)
+                }
+                .padding()
+//                Text("SocialView")
+                
+                Spacer()
+                
             }
+            .tag(0)
+            
+            Text("SocialView")
+                .tag(1)
         }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+        
     }
 }
 
@@ -30,5 +52,20 @@ struct SocialView: View {
 struct SocialView_Previews: PreviewProvider {
     static var previews: some View {
         SocialView()
+    }
+}
+
+@available(iOS 14.0, *)
+struct SocialPage: View {
+    var body: some View {
+        NavigationView {
+            Text("SocialView")
+                .navigationTitle("Social")
+                .navigationBarItems(trailing: Button(action: {}) {
+                    Image(systemName: "paperplane")
+                        .font(.title)
+                        .foregroundColor(.primary)
+                })
+        }
     }
 }
