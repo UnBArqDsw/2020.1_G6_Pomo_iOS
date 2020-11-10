6f0335cf16a147ed2dfb0ca9b85fd3b300de0b5e
João Gabriel Antunes <jg.antunes.medeiros@gmail.com>
2020-11-10 16:44:43 -0300

    [FEATURE]UnBArqDsw/2020.1_G6_Pomo#52 Adicionando charts ao StatsView.

--- Pomo/Pomo/View/StatsView.swift
+++ Pomo/Pomo/View/StatsView.swift
@@ -7,14 +7,35 @@
 //
 
 import SwiftUI
+import SwiftUICharts
 
 @available(iOS 14.0, *)
 struct StatsView: View {
+    @FetchRequest(fetchRequest: SessionItem.getAllSessionItems()) var sessionItems: FetchedResults<SessionItem>
+    
+    var columns = [
+        GridItem(.adaptive(minimum: 120)),
+        GridItem(.adaptive(minimum: 120))
+    ]
+    
     var body: some View {
         NavigationView {
-            Text("Stats Go Here!")
-            
+            ScrollView {
+                VStack {
+                    Text("Stats Go Here!")
+                    LazyVGrid(columns: columns) {
+                        ForEach(sessionItems, id: \.self) { item in
+                            LineChartView(data: [10, 12, 9, 32, 45, 10, 11], title: item.name!, legend: item.sessionDescription!, dropShadow: false)
+                                .padding()
+                                
+                        }
+                    }
+                    
+                        
+                }
                 .navigationTitle("Stats")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
         }
     }
 }
